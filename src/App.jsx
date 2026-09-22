78c0c6d7496586df2ff9854afcc7103c98b3f6f6
blueeyespark <fallenangeljr1@gmail.com>
2026-09-22 06:52:37 -0600

    feat(academy): route first course room preview

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ import MusicEditor from './pages/MusicEditor';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import AcademyHome from './pages/AcademyHome';
+import AcademyCourseRoom from './pages/AcademyCourseRoom';
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
@@ -48,6 +49,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/Academy" element={<LayoutWrapper currentPageName="Academy"><AcademyHome /></LayoutWrapper>} />
+      <Route path="/Academy/Courses/3d-modeling-fundamentals" element={<LayoutWrapper currentPageName="Academy"><AcademyCourseRoom /></LayoutWrapper>} />
 
       <Route path="/" element={
         <LayoutWrapper currentPageName={mainPageKey}>
