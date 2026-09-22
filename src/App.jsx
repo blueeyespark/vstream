a4af771907bc9902ba11ad4b355f8b853da69a6f
blueeyespark <fallenangeljr1@gmail.com>
2026-09-22 10:17:14 -0600

    feat(academy): route Student Center

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ import AcademyCourseRoom from './pages/AcademyCourseRoom';
 import AcademyLessonPreview from './pages/AcademyLessonPreview';
 import AcademyExplore from './pages/AcademyExplore';
 import AcademyCreatorCampus from './pages/AcademyCreatorCampus';
+import AcademyStudentCenter from './pages/AcademyStudentCenter';
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
@@ -54,6 +55,7 @@ const AppRoutes = () => {
       <Route path="/Academy" element={<LayoutWrapper currentPageName="Academy"><AcademyHome /></LayoutWrapper>} />
       <Route path="/Academy/Explore" element={<LayoutWrapper currentPageName="Academy"><AcademyExplore /></LayoutWrapper>} />
       <Route path="/Academy/CreatorCampus" element={<LayoutWrapper currentPageName="Academy"><AcademyCreatorCampus /></LayoutWrapper>} />
+      <Route path="/Academy/StudentCenter" element={<LayoutWrapper currentPageName="Academy"><AcademyStudentCenter /></LayoutWrapper>} />
       <Route path="/Academy/Courses/3d-modeling-fundamentals" element={<LayoutWrapper currentPageName="Academy"><AcademyCourseRoom /></LayoutWrapper>} />
       <Route path="/Academy/Courses/3d-modeling-fundamentals/module-1" element={<LayoutWrapper currentPageName="Academy"><AcademyLessonPreview /></LayoutWrapper>} />
 
